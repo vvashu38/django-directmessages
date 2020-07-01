1c079687c1c4ae89afbb14e180ed7d5105fa738b
Subodh Vashistha <43090551+vvashu38@users.noreply.github.com>
2020-07-01 19:20:33 +0530

    Update models.py

--- directmessages/models.py
+++ directmessages/models.py
@@ -4,7 +4,9 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 
-from django.utils.encoding import python_2_unicode_compatible
+#from django.utils.encoding import python_2_unicode_compatible
+from django.utils.encoding import smart_text
+from six import python_2_unicode_compatible
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
